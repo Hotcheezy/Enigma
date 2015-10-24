7f30758482c69cb208a8a0a3547005011681783a
Peter Leng <peterleng1234@gmail.com>
2015-10-25 07:49:23 +0800

    fixed the issue that prevents the program from returning to previous window

---
+++ src/settings/rotatorSettings/rotatorSettings.h
@@ -0,0 +1,17 @@
+//=======================================================================================================
+// Project: Enigma 1.0v
+// Description: Enigma Machine on Pebble Watch
+// File: The rotatorSettings.h file
+// Built using Pebble SDK 3.4v
+// Date: October 25, 2015
+//
+// Programmer: Peter Leng, Kent Liu
+// MIT Licence
+// ======================================================================================================
+
+#pragma once
+
+#include <pebble.h>
+
+// Pushed to the menu window
+void rotator_settings_push();
